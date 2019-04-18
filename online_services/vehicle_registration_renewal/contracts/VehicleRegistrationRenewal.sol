86791cd5d74822de3dc3e52dbe774c5f71839f25
smilanko <ilija402@gmail.com>
2019-04-17 18:26:00 -0700

    initial push

---
+++ online_services/vehicle_registration_renewal/contracts/VehicleRegistrationRenewal.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+contract VehicleRegistrationRenewal {
+
+	string public car;
+	string public person;
+
+	// constructor 
+	constructor() public {
+		car = "";
+		person = "";
+	}
+
+}
