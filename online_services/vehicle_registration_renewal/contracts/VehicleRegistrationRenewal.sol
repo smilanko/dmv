7cd431bf17404b450f7c273e45d6c6f4024e6112
smilanko <ilija402@gmail.com>
2019-04-17 18:58:10 -0700

    more progress

--- online_services/vehicle_registration_renewal/contracts/VehicleRegistrationRenewal.sol
+++ online_services/vehicle_registration_renewal/contracts/VehicleRegistrationRenewal.sol
@@ -2,13 +2,33 @@ pragma solidity ^0.5.0;
 
 contract VehicleRegistrationRenewal {
 
-	string public car;
-	string public person;
+	struct Vehicle {
+		uint vin;
+		uint year;
+		string model;
+	}
 
-	// constructor 
+	struct Owner {
+		uint ssn;
+		string first_name;
+		string last_name;
+	}
+
+	mapping(uint => Vehicle) public vehicles;
+	mapping(uint => Owner) public owners;
+
+	uint public ownerCount;
+	uint public vehicleCount;
+
 	constructor() public {
-		car = "";
-		person = "";
+		
+	}
+
+	function registerVehicle(uint _vin, uint _year, string memory _model, uint _ssn, string memory _firstName, string memory _lastName) private {
+		ownerCount++;
+		vehicleCount++;
+		vehicles[vehicleCount] = Vehicle(_vin, _year, _model);
+		owners[ownerCount] = Owner(_ssn, _firstName ,_lastName);
 	}
 
 }
