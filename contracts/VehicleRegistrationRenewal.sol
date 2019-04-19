121e2fa7be84592f94ecbbd0c1f5bf150d811a19
smilanko <ilija402@gmail.com>
2019-04-18 17:06:57 -0700

    added support to trade vehicles;

--- contracts/VehicleRegistrationRenewal.sol
+++ contracts/VehicleRegistrationRenewal.sol
@@ -31,4 +31,16 @@ contract VehicleRegistrationRenewal {
 		}
 	}
 
+	function reportSoldTraded(address _to) public {
+		if (isRegistrationPresent()) {
+			uint _car_vin = registrations[msg.sender].car_vin;
+			uint _car_year = registrations[msg.sender].car_year;
+			string memory _car_model = registrations[msg.sender].car_model;
+			uint _expiring_year = registrations[msg.sender].expiring_year;
+			registrations[msg.sender] = Registration(0, 0, "", 0);
+			registrations[_to] = Registration(_car_vin, _car_year, _car_model, _expiring_year);
+			emit registrationEvent(_car_vin);
+		}
+	}
+
 }
--- test/VehicleRegistrationRenewal.js
+++ test/VehicleRegistrationRenewal.js
@@ -32,5 +32,36 @@ contract("VehicleRegistrationRenewal", function(accounts){
 		});
 	});
 
+	it("vehicle transferred", function() {
+		return VehicleRegistrationRenewal.deployed().then(function(instance) {
+			registrationInstance = instance;
+			vin = 123;
+			year = 1990;
+			model = "toyota";
+			return registrationInstance.processRegistration(vin, year, model, { from : accounts[0] });
+		}).then(function(receipt) {
+			return registrationInstance.isRegistrationPresent({ from : accounts[0] });
+		}).then(function(registered) {
+			assert(registered, "user marked as registered car");
+			return registrationInstance.reportSoldTraded(accounts[1]);
+		}).then(function(updated_recepipt) {
+			return registrationInstance.isRegistrationPresent({ from : accounts[0] });
+		}).then(function(updated_registration) {
+			assert.isFalse(updated_registration, "original no longer registered");
+			return registrationInstance.registrations(accounts[1]);
+		}).then(function(registrations) {
+			assert.equal(123, registrations[0], "vin match");
+			assert.equal(1990, registrations[1], "year match");
+			assert.equal("toyota", registrations[2], "model match");
+			assert.equal(new Date().getFullYear(), registrations[3], "expiring year match");
+			return registrationInstance.registrations(accounts[0]);
+		}).then(function(registrations) {
+			assert.equal(0, registrations[0], "vin match");
+			assert.equal(0, registrations[1], "year match");
+			assert.equal("", registrations[2], "model match");
+			assert.equal(0, registrations[3], "expiring year match");
+		});
+	});
+
 
 });
